0730861ec505ac728206facb3276e5129ff3d598
shota <key.sk35@gmail.com>
2018-10-28 00:47:54 +0900

    fix: axiosの追加 & actionsの追加

--- src/store/types.js
+++ src/store/types.js
@@ -1,1 +1,2 @@
-export const MESSAGE_UPDATE = 'MESSAGE_UPDATE';
+export const MESSAGE_UPDATE = "MESSAGE_UPDATE";
+export const SET_USER = "SET_USER";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,19 @@
       {{ message }}
     </div>
     <input type="button" value="名前は" @click="update()">
+
+    <div>
+      <input type="button" value="ユーザ取得" @click="getUsers()">
+      <div v-for="user in users" :key="user.id">
+        ID: {{ user.id }} 名前: {{ user.name }} 年齢: {{ user.age }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
-import { mapState, mapMutations } from "vuex";
+import { mapState, mapMutations, mapActions } from "vuex";
 import { MESSAGE_UPDATE } from "../store/types";
 export default {
   name: "home",
@@ -21,12 +28,16 @@ export default {
   },
   computed: {
     ...mapState({
-      message: 'message'
+      message: "message",
+      users: "users"
     })
   },
   methods: {
     ...mapMutations({
       MESSAGE_UPDATE
+    }),
+    ...mapActions({
+      getUsers: "getUsers"
     }),
     update() {
       this.MESSAGE_UPDATE(); // mutationをコールします
